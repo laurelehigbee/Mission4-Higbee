72e3ba2ecc5c26fc8c8bb6331a18b884d5fad63b
laurelehigbee <90651214+laurelehigbee@users.noreply.github.com>
2022-01-26 17:16:51 -0700

    Add project files.

---
+++ Mission4-Higbee/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission4_Higbee.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_Higbee.Controllers
+{
+    public class HomeController : Controller //controller creation
+    {
+        private readonly ILogger<HomeController> _logger;
+        private Context _aVarName { get; set; } //connects to the context model
+
+        public HomeController(ILogger<HomeController> logger, Context randomName) 
+            //information to connect to the database, context model
+        {
+            _logger = logger;
+            _aVarName = randomName;
+        }
+
+        public IActionResult Index() //return view for index page
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddMovies() //get response for add movies web page
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMovies(MovieResponse mr) //post response for add movies
+        {
+            _aVarName.Add(mr); //adds the information into the response
+            _aVarName.SaveChanges(); //saves the changes to the database
+            return View("Confirmation",mr); //returns the confirmation view, movie response info saved
+        }
+
+        public IActionResult PodcastInfo() //return view for podcast info page
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() //return view for if there's an error
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
